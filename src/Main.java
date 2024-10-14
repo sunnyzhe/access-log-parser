97998f9b70487aa3b21ec9844dae8ea637fd7fd9
Evgeniia Patrikeeva <sunny.zhe@mail.ru>
2024-10-14 22:13:57 +0500

    add division,sum etc.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число");
+        int number= new Scanner(System.in) .nextInt();
+        System.out.println("Введите второе число");
+        int number2= new Scanner(System.in) .nextInt();
+        double division= (double) number2/number;
+        System.out.println("Сумма равна "+(number2+number));
+        System.out.println("Разность  равна "+(number2-number));
+        System.out.println("Произведение чисел "+(number2*number));
+        System.out.println("Частное чисел " +division);
     }
 }
